b131269a7c0bbddf303cde3816da8987a7340722
KeiNJL <nikshilov32@gmail.com>
2023-09-29 17:22:01 +0300

    One LED 2_0

--- Core/Src/LedProc.c
+++ Core/Src/LedProc.c
@@ -17,7 +17,6 @@ void Blink(void)
 {
 	uint32_t current_time = HAL_GetTick();
 	uint32_t previous_time = 0;
-	uint8_t value = 0b0;
 
 		if ((current_time - previous_time) > 1000)
 		{
@@ -26,20 +25,18 @@ void Blink(void)
 			if (value&0b00001 == 0b00000)
 			{
 				value = value | 1;
-				value << 1;
 			}
 			else
 			{
 				value = value | 0;
-				value << 1;
 			}
+			value << 1;
 		}
 }
 void FastBlink(void)
 {
 	uint32_t current_time = HAL_GetTick();
 	uint32_t previous_time = 0;
-	uint8_t value = 0b0;
 
 		if ((current_time - previous_time) > 500)
 		{
@@ -47,14 +44,13 @@ void FastBlink(void)
 			previous_time = current_time;
 			if (value&0b00001 == 0b00000)
 			{
-				value = value | 1;
-				value << 1;
+				value = value ^ 1;
 			}
 			else
 			{
-				value = value | 0;
-				value << 1;
+				value = value ^ 1;
 			}
+			value << 1;
 		}
 }
 
